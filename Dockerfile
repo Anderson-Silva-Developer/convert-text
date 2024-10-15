9cfc20cda27ad6b5d355cc30d52643ff03f14ee7
ascosta3 <ascosta3@stefanini.com>
2024-10-14 21:05:16 -0300

    feat: adiccionado base64

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,28 @@
+# Dockerfile
+
 FROM ubuntu:latest AS build
 
 RUN apt-get update && apt-get install -y \
     openjdk-21-jdk \
     wget \
-    unzip
+    unzip \
+    python3 \
+    python3-pip \
+    python3-venv
 
-# Install Gradle 8.2.1
+# Criar e ativar um ambiente virtual
+RUN python3 -m venv /opt/venv
+ENV PATH="/opt/venv/bin:$PATH"
+
+# Instalar yt-dlp no ambiente virtual
+RUN pip install yt-dlp
+
+# Instalar Gradle 8.2.1
 RUN wget https://services.gradle.org/distributions/gradle-8.2.1-bin.zip -P /tmp
 RUN unzip -d /opt/gradle /tmp/gradle-8.2.1-bin.zip
 RUN ln -s /opt/gradle/gradle-8.2.1 /opt/gradle/latest
 ENV GRADLE_HOME=/opt/gradle/latest
-ENV PATH=${GRADLE_HOME}/bin:${PATH}
+ENV PATH="${GRADLE_HOME}/bin:${PATH}"
 
 COPY . .
 
--- src/main/java/br/com/anderson/silva/apigetvideo/service/VideoService.java
+++ src/main/java/br/com/anderson/silva/apigetvideo/service/VideoService.java
@@ -6,7 +6,12 @@ import br.com.anderson.silva.apigetvideo.dto.VideoDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
 import java.time.Duration;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,9 +25,9 @@ public class VideoService {
         this.youtubeClient = youtubeClient;
     }
 
-    public Map<String, String> getVideo(String url)  {
+    public Map<String, String> getVideo(String url) {
         String videoId = url.split("v=")[1];
-        VideoDTO video = youtubeClient.search("snippet,contentDetails", videoId,key);
+        VideoDTO video = youtubeClient.search("snippet,contentDetails", videoId, key);
         Map<String, String> response = new HashMap<>();
 
         Item item = video.getItems().get(0);
@@ -34,9 +39,43 @@ public class VideoService {
         response.put("duration", String.valueOf(minutes) + " : " + String.valueOf(seconds));
         response.put("title", item.getSnippet().getTitle());
         response.put("imgVideo", item.getSnippet().getThumbnails().getMedium().getUrl());
+        try {
+            String videoBase64 = response.put("video", downloadAndConvertToBase64(item.getId()));
+            response.put("video", videoBase64);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return response;
 
+    }
+
+    public String downloadAndConvertToBase64(String youtubeUrl) throws Exception {
+        String videoUrl = youtubeUrl;
+        String outputFileName = "video.mp4";
+        String ytDlpPath = "/opt/venv/bin/yt-dlp";
+
+        try {
+            // Baixar o vídeo usando yt-dlp
+            ProcessBuilder builder = new ProcessBuilder(
+                    ytDlpPath,
+                    "-o", outputFileName,
+                    videoUrl
+            );
+            Process process = builder.start();
+            process.waitFor();
+
+            // Ler o arquivo MP4 e codificar em Base64
+            File videoFile = new File(outputFileName);
+            byte[] fileContent = Files.readAllBytes(videoFile.toPath());
+            String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+            System.out.println("Base64 Encoded Video: " + encodedString);
+            return encodedString;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
 
